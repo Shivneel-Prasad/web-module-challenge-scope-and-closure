d5312fc2e71507fc5e81138be7b2b7d9330fa6f3
Shivneel-Prasad <shivam201020@gmail.com>
2021-08-16 12:49:58 -0700

    Completed task 2 to 4

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
+      - 
   
   2. Which of the two uses a closure? How can you tell?
   
@@ -61,10 +62,12 @@ Use the inning function below to do the following:
   
 NOTE: This will be a callback function for the tasks below
 */
-
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+  return Math.floor(Math.random() * Math.floor(3));
 }
+
+console.log('Task 2:',inning());
+
 
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
@@ -81,18 +84,36 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(inningCB, number){
+  let homeScore = 0;
+  let awayScore = 0;
+
+  for(let i = 0; i < number; i++){
+      homeScore = homeScore + inning();
+      awayScore = awayScore + inning();
+  }
+  return {
+      "Home": homeScore,
+      "Away": awayScore
+  }
 }
+
+console.log('Task 3:',finalScore(inning, 9));
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
-
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+// 
+// returning an object with 
+function getInningScore(inningcb) {
+    return {
+      "Home": inning(),
+      "Away": inning()
+  }
 }
+
+console.log('Task 4:',getInningScore());
 
 
 /* ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
@@ -135,7 +156,7 @@ Use the scoreboard function below to do the following:
   "This game will require extra innings: Away 10 - Home 10"
 ]  
   */
-
+// inningscorecb, scorecb, number
 function scoreboard(/* CODE HERE */) {
   /* CODE HERE */
 }
